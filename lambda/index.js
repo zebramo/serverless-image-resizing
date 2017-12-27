a4171e0e5be82ab762f66ba9940b0ac08c21baea
Mehmet Fatih Köksal <fatihkoksal@gmail.com>
2017-12-27 15:38:56 +0100

    Width and height are at the end of the image path

--- lambda/index.js
+++ lambda/index.js
@@ -11,15 +11,16 @@ const URL = process.env.URL;
 
 exports.handler = function(event, context, callback) {
   const key = event.queryStringParameters.key;
-  const match = key.match(/(\d+)x(\d+)\/(.*)/);
-  const width = parseInt(match[1], 10);
-  const height = parseInt(match[2], 10);
-  const originalKey = match[3];
+  const match = key.match(/(.*)\_(\d+)x(\d+)\.(.*)/);
+  const path = match[1];
+  const width = parseInt(match[2], 10);
+  const height = parseInt(match[3], 10);
+  const extension = match[4];
+  const originalKey = path + "." + extension;
 
   S3.getObject({Bucket: BUCKET, Key: originalKey}).promise()
     .then(data => Sharp(data.Body)
       .resize(width, height)
-      .toFormat('png')
       .toBuffer()
     )
     .then(buffer => S3.putObject({
